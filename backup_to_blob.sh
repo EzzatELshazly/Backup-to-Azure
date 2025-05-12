a3061c6b0b0bedf89f803d64f04a7477d7e96912
EzzatELshazly <65233473+EzzatELshazly@users.noreply.github.com>
2025-05-12 16:46:15 +0300

    Update backup_to_blob.sh

--- backup_to_blob.sh
+++ backup_to_blob.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+# Variables
 SOURCE_FILE="/var/www/frontuat/db.sqlite3"
 BACKUP_NAME="db-$(date +'%Y-%m-%d-%H%M%S').sqlite3"
-CONTAINER_URL="https://XXXXXXX.blob.core.windows.net/#blob-name/${BACKUP_NAME}?sp=rawd&st=2025-05-08T15:06:11Z&se=2035-05-08T23:06:11Z&sv=2024-11-04&sr=c&sig=ck98IBGQl2w0n9wHAC9dCyejncDYL1kldSD9An%2BNT3U%3D"
+CONTAINER_URL="https://<account>.blob.core.windows.net/backups/${BACKUP_NAME}?sp=rawd&st=2025-05-08T15:06:11Z&se=2035-05-08T23:06:11Z&sv=2024-11-04&sr=c&sig=<signature>"
 LOG_FILE="/var/log/backup.log"
+
+# Ensure log file exists
+touch "$LOG_FILE"
 
 # Log start
 echo "Backup started at $(date)" >> "$LOG_FILE"
